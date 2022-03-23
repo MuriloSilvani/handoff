17d21c8f7e1b5b3f27168af1dfd5f2bd3b96611d
Murilo Silvani <murilo@amo.delivery>
2022-03-23 15:19:45 -0300

    lint, babel, styled

--- src/components/Commom/Commom.tsx
+++ src/components/Commom/Commom.tsx
@@ -1,13 +1,16 @@
-import styles from './Commom.module.css'
-
 import { Header } from '../Header'
 import { Menu } from '../Menu'
+
+import {
+  Container
+} from './Commom'
 
 export function Commom() {
   return (
     <>
       <Header />
       <Menu />
+      <Container />
     </>
   )
 }
